201b4349384935b3892946dc19ae44d7812c3982
AVOtekktion <91689292+AnriCzarnowski@users.noreply.github.com>
2021-11-02 00:22:34 +0100

    Rysowanie trójkąta ze znaków "X"

---
+++ ZADANIE3/Program.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ZADANIE3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Podaj n");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int wiersz = 0; wiersz <= n; wiersz++)
+            {
+                int lwierszy = n;
+                for (int kolumna = 0; kolumna <= n
+                    ; kolumna++)
+                {
+                    int lkolumn = n;
+                    if ((kolumna == lwierszy) || (wiersz == lwierszy) || (kolumna == lkolumn - wiersz))
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();   
+            
+            
+        }
+    }
+}
