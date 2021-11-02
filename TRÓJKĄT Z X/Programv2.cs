fa4574be1490808a61a07d93701fd7f2f4b4f91f
AVOtekktion <91689292+AnriCzarnowski@users.noreply.github.com>
2021-11-02 23:02:52 +0100

    Rysowanie pełnego trójkąta ze znaków "X"

---
+++ TRÓJKĄT Z X/Programv2.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ZADANIE3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Podaj n");
+            n = Convert.ToInt32(Console.ReadLine());
+            n--;
+            for (int wiersz = 0; wiersz <= n; wiersz++)
+            {
+                int lwierszy = n;
+                for (int kolumna = 0; kolumna <= n; kolumna++)
+                {
+                    int lkolumn = n;
+                    if  (kolumna >= lkolumn - wiersz)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
