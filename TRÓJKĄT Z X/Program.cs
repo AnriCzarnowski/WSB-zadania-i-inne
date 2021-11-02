27083ea1549028676fb7166450d071073d05bc67
AVOtekktion <91689292+AnriCzarnowski@users.noreply.github.com>
2021-11-02 10:02:08 +0100

    Rysowanie Trójkąta z "X" w .NET

--- TRÓJKĄT Z X/Program.cs
+++ TRÓJKĄT Z X/Program.cs
@@ -9,11 +9,11 @@ namespace ZADANIE3
             int n;
             Console.WriteLine("Podaj n");
             n = Convert.ToInt32(Console.ReadLine());
+            n--;
             for (int wiersz = 0; wiersz <= n; wiersz++)
             {
                 int lwierszy = n;
-                for (int kolumna = 0; kolumna <= n
-                    ; kolumna++)
+                for (int kolumna = 0; kolumna <= n; kolumna++)
                 {
                     int lkolumn = n;
                     if ((kolumna == lwierszy) || (wiersz == lwierszy) || (kolumna == lkolumn - wiersz))
